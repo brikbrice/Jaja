6ebec5a1f43b7c3343e79a273aa90536e36ee4a1
brikbrice <brikbrice@gmail.com>
2022-03-05 00:30:09 +0100

    working simple server
    scrap the code from https://developer.mozilla.org/en-US/docs/Learn/Server-side/Node_server_without_framework

--- simpleServer.js
+++ simpleServer.js
@@ -1,25 +1,49 @@
 const http = require('http');
 const fs = require('fs');
+const path = require('path')
 
-/*
-const mime = {
-    '.js': 'text/javascript; charset=UTF-8',
-    '.txt': 'text/plain; charset=UTF-8',
-    '.html': 'text/html; charset=UTF-8',
-    '.png': 'image/png',
-    '.gif': 'image/gif',
-    '.jpg': 'image/jpeg'
-  }
-*/
 
-http.createServer(function (req, res) {
-    fs.readFile('index.html', (err,data)=>{
-        if (err) throw err
-        res.writeHead(200, {'Contetnt-Type': 'text/html'})
-        res.end(data)
-        return
+
+
+http.createServer(function (request, response) {
+    console.log('request', request.url)
+
+    let filePath = '.' + request.url;
+    if ( filePath == './'){
+         filePath = './index.html'
+    }
+
+    let extname =
+    String(path.extname(filePath)).toLowerCase()
+    let mimeTypes = {
+        '.html': 'text/html',
+        '.js': 'text/javascript',
+        '.css': 'text/css',
+        '.json': 'application/json',
+        '.png': 'image/png',
+        '.jpg': 'image/jpg',
+        '.gif': 'image/gif',
+        '.svg': 'image/svg+xml',
+        '.wav': 'audio/wav',
+        '.mp4': 'video/mp4',
+        '.woff': 'application/font-woff',
+        '.ttf': 'application/font-ttf',
+        '.eot': 'application/vnd.ms-fontobject',
+        '.otf': 'application/font-otf',
+        '.wasm': 'application/wasm'
+    }
+
+    let contentType = mimeTypes[extname] || 'application/octet-stream'
+
+    fs.readFile(filePath, function(error, content){
+        if (error) throw error
+        else {
+            response.writeHead(200, { 'Content-Type':contentType})
+            response.end(content, 'utf-8')
+        }
     })
-}).listen(8080) 
 
-console.log('Server running at')
+}).listen(8080)
+console.log('server at http://localhost:8080')
 
+// faire un server qui inspect un dossier et qui créé une liste pour chaque fichier trouvé 
